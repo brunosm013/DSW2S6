3710d04abd824392695ffd488699a1df820936c9
Bruno Siqueira <brunosm013@gmail.com>
2018-08-09 14:03:56 -0300

    commit

--- compras/src/main/java/br/edu/ifsp/arq/dw2/compras/Checkout.java
+++ compras/src/main/java/br/edu/ifsp/arq/dw2/compras/Checkout.java
@@ -1,11 +1,13 @@
 package br.edu.ifsp.arq.dw2.compras;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Checkout {
 	@Autowired
+	@Qualifier("impressoraHP")
 	private Impressora impressora;
 
 	public void finalizar() {
